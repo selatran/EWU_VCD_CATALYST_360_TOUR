f8e2ab76b52fed52472c0542042a2360803b7414
selatran <80890760+selatran@users.noreply.github.com>
2022-03-08 22:05:27 -0800

    add files

---
+++ app-files/data.js
@@ -0,0 +1,755 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-front-view-of-catalyst-building",
+      "name": "Front View of Catalyst Building",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.17276178832088007,
+        "pitch": -0.06904279239221012,
+        "fov": 1.4329342573807347
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5469241985364377,
+          "pitch": 0.02254913365814204,
+          "rotation": 0,
+          "target": "1-main-entrance1f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0456157752334185,
+          "pitch": -0.009137774296394952,
+          "title": "Parking",
+          "text": "Free parking for students are available around the school building.&nbsp;<div><br></div><div>For more information about student parking and bus routes visit: https://www.ewu.edu/spokane/</div>"
+        },
+        {
+          "yaw": 0.7272212334288444,
+          "pitch": 0.02433555321575298,
+          "title": "The Catalyst Building",
+          "text": "The new Catalyst building, a joint project of Avista Development and McKinstry, provides Eastern students with modern, high tech-classrooms and immersive learning experiences."
+        }
+      ]
+    },
+    {
+      "id": "1-main-entrance1f",
+      "name": "Main Entrance–1F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8544101061575464,
+          "pitch": 0.05088281023368424,
+          "rotation": 0,
+          "target": "0-front-view-of-catalyst-building"
+        },
+        {
+          "yaw": -2.7783625719103,
+          "pitch": 0.014948334010739472,
+          "rotation": 0,
+          "target": "2-inside-main-entrance1f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.596200446058985,
+          "pitch": 0.0214094969411871,
+          "title": "Main Entrance",
+          "text": "This is the main entrance access to all design classes, located on the second floor and design events, located on the first floor towards the back in the Innovation Hub."
+        }
+      ]
+    },
+    {
+      "id": "2-inside-main-entrance1f",
+      "name": "Inside Main Entrance–1F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.337205046105149,
+          "pitch": 0.07049608855990641,
+          "rotation": 0,
+          "target": "4-west-side1f"
+        },
+        {
+          "yaw": 2.129024393107273,
+          "pitch": 0.09904257953034445,
+          "rotation": 0,
+          "target": "3-east-side1f"
+        },
+        {
+          "yaw": -2.4766656406345646,
+          "pitch": 0.02270718810624217,
+          "rotation": 0,
+          "target": "1-main-entrance1f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.125076295377742,
+          "pitch": 0.27558251308004955,
+          "title": "Restrooms &amp; Drinking Fountain",
+          "text": "Need to use the restoom or rehydrate yourself? Take a right turn here when you enter the main entrance to freshen up!<div><br></div><div>Additional restrooms and drinking fountains on upper floors.</div>"
+        },
+        {
+          "yaw": -1.1929649037146355,
+          "pitch": 0.08020020434989306,
+          "title": "Lobby to Innovation Hub",
+          "text": "Need a place to rest and study? There are many spots to sit down and relax before your next class."
+        },
+        {
+          "yaw": 0.7205705899762389,
+          "pitch": 0.09907434155288897,
+          "title": "Gender Neutral Restroom",
+          "text": "Located on the right of this narrow hallway is a gender neutral restroom for use."
+        }
+      ]
+    },
+    {
+      "id": "3-east-side1f",
+      "name": "East Side–1F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2332644309798209,
+          "pitch": 0.12283019325720801,
+          "rotation": 0,
+          "target": "2-inside-main-entrance1f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9603151940359096,
+          "pitch": 0.05517682535006685,
+          "title": "Restooms",
+          "text": "Located down this hall to the right."
+        }
+      ]
+    },
+    {
+      "id": "4-west-side1f",
+      "name": "West Side–1F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.419877555769685,
+          "pitch": 0.06200904228782633,
+          "rotation": 0,
+          "target": "2-inside-main-entrance1f"
+        },
+        {
+          "yaw": 0.45066902323123514,
+          "pitch": 0.1057988455984642,
+          "rotation": 0,
+          "target": "5-west-side-lobby-1f"
+        },
+        {
+          "yaw": 1.0782666026188785,
+          "pitch": -0.21977537187370189,
+          "rotation": 0,
+          "target": "7-top-of-stairs2f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6214822465291618,
+          "pitch": 0.10956138266220883,
+          "title": "Lobby &amp; Innovation Hub",
+          "text": "Lobby and Innovation Hub are located towards the back of the 1st floor.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "5-west-side-lobby-1f",
+      "name": "West Side Lobby-1F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4565051520049455,
+          "pitch": 0.03272659045547499,
+          "rotation": 0,
+          "target": "4-west-side1f"
+        },
+        {
+          "yaw": 2.933620667112643,
+          "pitch": 0.041679338387886844,
+          "rotation": 0,
+          "target": "6-innovation-hub1f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.1163236308291946,
+          "pitch": 0.04383364691746472,
+          "title": "The Innovation Hub",
+          "text": "Additional lobby area located in the Innovation Hub. Most design events and activities will be located in this space."
+        }
+      ]
+    },
+    {
+      "id": "6-innovation-hub1f",
+      "name": "Innovation Hub–1F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.23131714031693562,
+          "pitch": 0.11172552727021845,
+          "rotation": 0,
+          "target": "5-west-side-lobby-1f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.665441001426931,
+          "pitch": -0.002253766291609338,
+          "title": "Design Events",
+          "text": "Enter through these doors to attend design events."
+        }
+      ]
+    },
+    {
+      "id": "7-top-of-stairs2f",
+      "name": "Top of Stairs–2F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7135467687614527,
+          "pitch": 0.6931445855814289,
+          "rotation": 0,
+          "target": "4-west-side1f"
+        },
+        {
+          "yaw": 1.4509874537959098,
+          "pitch": 0.0218577524114707,
+          "rotation": 0,
+          "target": "8-cs-areaeast-side2f"
+        },
+        {
+          "yaw": -2.967263847944075,
+          "pitch": 0.07650273236588134,
+          "rotation": 0,
+          "target": "9-lobby2f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.7825955107161278,
+          "pitch": 0.07139196763091427,
+          "title": "VCD Classrooms",
+          "text": "All design classes will take place on this floor on the west side of the second floor. There is also a lobby located to right.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "8-cs-areaeast-side2f",
+      "name": "CS Area–East Side–2F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.1424696897823932,
+          "pitch": 0.0977218867730052,
+          "title": "CS Area &amp; Food Pantry",
+          "text": "Computer Science classrooms and teaching labs.&nbsp;<div><br></div><div>Need a quick snack. The food pantry located towards the back is free to use for all students.</div>"
+        }
+      ]
+    },
+    {
+      "id": "9-lobby2f",
+      "name": "Lobby–2F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9419588987933594,
+          "pitch": 0.1072037916963815,
+          "rotation": 0,
+          "target": "7-top-of-stairs2f"
+        },
+        {
+          "yaw": 1.714058155059984,
+          "pitch": 0.06725441140032373,
+          "rotation": 0,
+          "target": "10-lobby-end2f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.907151054768569,
+          "pitch": 0.0698555200654205,
+          "title": "Lobby",
+          "text": "Natural lights and plenty of sitting area to do work or hang out with your friends!"
+        }
+      ]
+    },
+    {
+      "id": "10-lobby-end2f",
+      "name": "Lobby End–2f",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.536041897310138,
+          "pitch": 0.034693267385893733,
+          "rotation": 0,
+          "target": "9-lobby2f"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-vcd-area-2f",
+      "name": "VCD Area-2F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1803663420421273,
+          "pitch": 0.11921145738098637,
+          "rotation": 0,
+          "target": "9-lobby2f"
+        },
+        {
+          "yaw": -1.908991882896789,
+          "pitch": 0.0712142512477385,
+          "rotation": 0,
+          "target": "12-vcd-right-hallway-2f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.913600211951854,
+          "pitch": 0.23159189574229622,
+          "title": "VCD Hallway",
+          "text": "All design courses will take place in this hallway on the right. There are restrooms, drinking fountains, and printers available here."
+        }
+      ]
+    },
+    {
+      "id": "12-vcd-right-hallway-2f",
+      "name": "VCD Right Hallway-2F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2360771281850127,
+          "pitch": 0.052965552420371154,
+          "rotation": 0,
+          "target": "11-vcd-area-2f"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-vcd-right-hallway-mid-2f",
+      "name": "VCD Right Hallway Mid-2F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09761120345186569,
+          "pitch": 0.04444267028137716,
+          "rotation": 0,
+          "target": "12-vcd-right-hallway-2f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6966125737154876,
+          "pitch": 0.10755169704871648,
+          "title": "Sitting Area",
+          "text": "Additional spaces to relax."
+        },
+        {
+          "yaw": 1.3984610718372767,
+          "pitch": 0.03326564418851774,
+          "title": "Left Hallway",
+          "text": "Electrical Engineering courses are located on the other side of this hallway."
+        },
+        {
+          "yaw": -2.984335992254179,
+          "pitch": -0.0350789302402994,
+          "title": "Printing",
+          "text": "<div>Onsite printing here. Additional printers are available on the third floor along with equipment checkouts.</div><div><br></div>EWU gives all students $9.00 worth of printing services."
+        }
+      ]
+    },
+    {
+      "id": "14-vcd-right-hallway-end-2f",
+      "name": "VCD Right Hallway End-2F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20191349296311678,
+          "pitch": 0.04593067489197189,
+          "rotation": 0,
+          "target": "13-vcd-right-hallway-mid-2f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.413063371475202,
+          "pitch": -0.022823761843165258,
+          "title": "Lobby",
+          "text": "Additional lobby area to relax or study."
+        }
+      ]
+    }
+  ],
+  "name": "Catalyst Building (VCD Floors) ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
